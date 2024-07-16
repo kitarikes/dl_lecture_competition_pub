41d5b6fd16d6691e8b1366405ff843f6ff271766
kitarikes <ryosuke092413@gmail.com>
2024-07-16 22:37:54 +0900

    f

--- job/run.sh
+++ job/run.sh
@@ -3,4 +3,4 @@
 #PBS -j oe
 #PBS -l select=1:ncpus=1:ngpus=1:host=aurum-g01
 
-docker exec rkita-vqa /bin/bash -c "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.3/compat && source /opt/miniconda/bin/activate vqa-env && python main.py > log2.log"
+docker exec condescending_hermann /bin/bash -c "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-12/compat && source /opt/miniconda/bin/activate vqa-env && python main.py > log2.log"
