0ddeedf82f8248f041c8230bd0262510debe7841
kitarikes <ryosuke092413@gmail.com>
2024-07-16 16:22:37 +0900

    fix

--- job/run.sh
+++ job/run.sh
@@ -3,4 +3,4 @@
 #PBS -j oe
 #PBS -l select=1:ncpus=1:ngpus=1:host=aurum-g01
 
-docker exec -it -v $PWD:/workspace -w /workspace rkita-vqa bash -c "exec $SHELL -l && conda activate vqa-env && python main.py" > log_vqa.txt
+docker exec -it rkita-vqa bash -c "exec \$SHELL -l && conda activate vqa-env && python main.py" > log.txt
