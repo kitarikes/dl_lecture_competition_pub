62923eb5a4cce35771ed92e4929a09a3390210b8
kitarikes <ryosuke092413@gmail.com>
2024-07-17 12:06:40 +0900

    update: module

---
+++ src/excute.py
@@ -0,0 +1,51 @@
+import time
+import numpy as np
+from .models import VQA_criterion
+
+# 4. 学習の実装
+def train(model, dataloader, optimizer, criterion, device):
+    model.train()
+
+    total_loss = 0
+    total_acc = 0
+    simple_acc = 0
+
+    start = time.time()
+    for image, question, answers, mode_answer in dataloader:
+        image, question, answer, mode_answer = \
+            image.to(device), question.to(device), answers.to(device), mode_answer.to(device)
+
+        pred = model(image, question)
+        loss = criterion(pred, mode_answer.squeeze())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        total_acc += VQA_criterion(pred.argmax(1), answers)  # VQA accuracy
+        simple_acc += (pred.argmax(1) == mode_answer).float().mean().item()  # simple accuracy
+
+    return total_loss / len(dataloader), total_acc / len(dataloader), simple_acc / len(dataloader), time.time() - start
+
+
+def eval(model, dataloader, optimizer, criterion, device):
+    model.eval()
+
+    total_loss = 0
+    total_acc = 0
+    simple_acc = 0
+
+    start = time.time()
+    for image, question, answers, mode_answer in dataloader:
+        image, question, answer, mode_answer = \
+            image.to(device), question.to(device), answers.to(device), mode_answer.to(device)
+
+        pred = model(image, question)
+        loss = criterion(pred, mode_answer.squeeze())
+
+        total_loss += loss.item()
+        total_acc += VQA_criterion(pred.argmax(1), answers)  # VQA accuracy
+        simple_acc += (pred.argmax(1) == mode_answer).mean().item()  # simple accuracy
+
+    return total_loss / len(dataloader), total_acc / len(dataloader), simple_acc / len(dataloader), time.time() - start
